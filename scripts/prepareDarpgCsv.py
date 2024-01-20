88ae788de0416d62445242516b69a2f65b772d3d
vkivaturi <vijay.ivaturi@gmail.com>
2024-01-20 12:57:08 +0530

    Merging grievance report and history files

--- scripts/prepareDarpgCsv.py
+++ scripts/prepareDarpgCsv.py
@@ -28,19 +28,43 @@ def remove_invalid_json_fields_from_history(input_file_name, output_file_name):
 	input_file.close()
 	output_file.close()
 
-def main():
-	print('## Starting file loads')
-	with open(report_json_file_name, encoding='utf8') as report_json_file:
-		report_data = json.load(report_json_file)
+def writeFinalCsv(report_data, history_dict, output_csv_file):
+	# Create the csv writer object
+	fieldnames = ['registration_no', 'org_code', 'dist_name', 'pincode', 'state', 'sex', 'history_id', 'history_officer_detail', 'history_org_code']
+	csv_writer = csv.DictWriter(output_csv_file, fieldnames=fieldnames)
 
 	print('## Grievance report file is loaded')
 	print("## Records in grievance file : ", len(report_data))
+
+	# Counter variable used for writing headers to the CSV file
+	count = 0
+	for record in report_data:
+		if count == 0:
+			count += 1
+			csv_writer.writeheader()
+
+		format_fields(record, fieldnames)
+
+    	#TODO - Below hard coding of field names can be optimized further
+		csv_writer.writerow({'registration_no': record['registration_no'], 
+					   'org_code': record['org_code'], 'dist_name': record['dist_name'], 
+					  'pincode': record['pincode'], 'state': record['state'], 'sex': record['sex'], 
+					  'history_id': history_dict.get(record['registration_no'])['_id'],
+					  'history_officer_detail': history_dict.get(record['registration_no'])['OfficerDetail'],
+					  'history_org_code': history_dict.get(record['registration_no'])['org_code']
+					  })
+
+def main():
+	print('## Starting file loads')
 
 	#Remove invalid json elements from history file
 	remove_invalid_json_fields_from_history(history_json_file_name, history_json_file_temp_name)
 
 	with open(history_json_file_temp_name, encoding='utf8') as history_json_file:
 		history_data = json.load(history_json_file)
+
+	with open(report_json_file_name, encoding='utf8') as report_json_file:
+		report_data = json.load(report_json_file)
 
 	print('## Grievance hisory file is loaded')
 	print('## Records in history_data : ', len(history_data))
@@ -54,36 +78,14 @@ def main():
 	print("## Records in history_dict : ", len(history_dict))
 
 	# Open file for writing
-	gievance_merged_file = open(gievance_merged_csv_file_name, 'w', encoding='utf8', newline='')
-
-	# Create the csv writer object
-	fieldnames = ['registration_no', 'org_code', 'dist_name', 'pincode', 'state', 'sex', 'history_id', 'history_officer_detail', 'history_org_code']
-	csv_writer = csv.DictWriter(gievance_merged_file, fieldnames=fieldnames)
-
-	# Counter variable used for writing headers to the CSV file
-	count = 0
-
-	for record in report_data:
-		if count == 0:
-			count += 1
-			csv_writer.writeheader()
-
-	    # Writing data of CSV file
-		format_fields(record, fieldnames)
-
-    	#TODO - Below hard coding of field names can be optimized further
-		csv_writer.writerow({'registration_no': record['registration_no'], 
-					   'org_code': record['org_code'], 'dist_name': record['dist_name'], 
-					  'pincode': record['pincode'], 'state': record['state'], 'sex': record['sex'], 
-					  'history_id': history_dict.get(record['registration_no'])['_id'],
-					  'history_officer_detail': history_dict.get(record['registration_no'])['OfficerDetail'],
-					  'history_org_code': history_dict.get(record['registration_no'])['org_code']
-					  })
-
+	output_csv_file = open(gievance_merged_csv_file_name, 'w', encoding='utf8', newline='')
+	
+	writeFinalCsv(report_data, history_dict, output_csv_file)
+	
 	#Cleanup of open file handlers
 	report_json_file.close()
 	history_json_file.close()
-	gievance_merged_file.close()
+	output_csv_file.close()
 
 if __name__ == "__main__":
     main()
